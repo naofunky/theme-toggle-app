8a481183abafd542c58db4d15aeaefec8c690e86
Koki Nanao <minsokosoko0107@gmail.com>
2023-07-01 23:29:12 +0900

    feat:追加
    テーマカラー追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
-import './globals.css'
+import ThemeSwitcher from './ThemeSwitcher';
+import './globals.css';
+import Providers from './provider';
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -12,7 +14,12 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <Providers>
+          <ThemeSwitcher />
+          {children}
+        </Providers>
+      </body>
     </html>
-  )
+  );
 }
